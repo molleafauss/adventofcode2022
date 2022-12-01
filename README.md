769936c28827b239ef87ca9f283268860a038c12
molleafauss <lmollea@yahoo.it>
2022-12-01 20:20:43 +0000

    Day 1 full solution

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# adventofcode2022
+# Advent Of Code 2022
+My solutions for advent of code 2022 (https://adventofcode.com/).
+
+All solutions are posted on the master branch no commits were cut. Feel free to go through the history.
--- src/01-calories.py
+++ src/01-calories.py
@@ -7,18 +7,19 @@ Q2) Find the top three Elves carrying the most Calories. How many Calories are t
 if __name__ == '__main__':
     elf = 0
     elf_calories = 0
-    max_calories = 0
+    calories = []
     with open("01-calories.txt") as f:
         for l in f:
             l = l.strip()
             if not l:
                 print("Elf n. {} holding {} calories".format(elf, elf_calories))
-                if elf_calories > max_calories:
-                    max_calories = elf_calories
+                calories.append([elf, elf_calories])
                 elf_calories = 0
                 elf += 1
                 continue
             elf_calories += int(l)
-    if elf_calories > max_calories:
-        max_calories = elf_calories
-    print("Saw {} elves: maximum: {}".format(elf, max_calories))
+    calories.append([elf, elf_calories])
+    calories.sort(key=lambda x: x[1], reverse=True)
+    print("Saw {} elves: maximum: {}".format(elf, calories[0]))
+    top3 = [x[1] for x in calories[0:3]]
+    print("First 3 elves: {}".format(sum(top3)))
