83468f3468765f73e5893a78c8cbfc8afa42acb1
molleafauss <lmollea@yahoo.it>
2022-12-08 22:41:56 +0000

    day 8 full

--- src/day08.py
+++ src/day08.py
@@ -21,7 +21,7 @@ class Solution(Solver):
         # view from top
         visible = 0
         tree = self.matrix[0][col]["size"]
-        for row in range(1, self.height):
+        for row in range(1, self.height - 1):
             t = self.matrix[row][col]
             if t["size"] > tree:
                 visible += 1 if not t["visible"] else 0
@@ -29,7 +29,7 @@ class Solution(Solver):
                 tree = t["size"]
         # visible from bottom
         tree = self.matrix[-1][col]["size"]
-        for row in range(-1, -self.width, -1):
+        for row in range(self.width - 1, 0, -1):
             t = self.matrix[row][col]
             if t["size"] > tree:
                 visible += 1 if not t["visible"] else 0
@@ -42,7 +42,7 @@ class Solution(Solver):
         # view from left
         visible = 0
         tree = self.matrix[row][0]["size"]
-        for col in range(1, self.width):
+        for col in range(1, self.width - 1):
             t = self.matrix[row][col]
             if t["size"] > tree:
                 visible += 1 if not t["visible"] else 0
@@ -50,7 +50,7 @@ class Solution(Solver):
                 tree = t["size"]
         # visible from right
         tree = self.matrix[row][-1]["size"]
-        for col in range(-1, -self.height, -1):
+        for col in range(self.height - 1, 0, -1):
             t = self.matrix[row][col]
             if t["size"] > tree:
                 visible += 1 if not t["visible"] else 0
@@ -60,6 +60,7 @@ class Solution(Solver):
         return visible
 
     def solve(self):
+        # part 1
         inside = 0
         self.height = len(self.matrix)
 
@@ -71,6 +72,41 @@ class Solution(Solver):
         # borders: 2 x (width + height) - 4 (corners, to not count them multiple times)
         borders = self.width * 2 + self.height * 2 - 4
         print(f"[1] number of trees visible {inside + borders}")
+
+        # part 2
+        scenic_max = 0
+        for row in range(1, self.height - 1):
+            for col in range(1, self.width - 1):
+                score = self.tree_score(row, col)
+                if score > scenic_max:
+                    scenic_max = score
+        print(f"[2] max scenic score {scenic_max}")
+
+    def tree_score(self, row, col):
+        score = 1
+        tree = self.matrix[row][col]
+        # go north
+        r = row-1
+        while r > 0 and self.matrix[r][col]["size"] < tree["size"]:
+            r -= 1
+        score *= abs(row - r)
+        # go south
+        r = row+1
+        while r < self.height - 1 and self.matrix[r][col]["size"] < tree["size"]:
+            r += 1
+        score *= abs(row - r)
+        # go east
+        c = col+1
+        while c < self.width - 1 and self.matrix[row][c]["size"] < tree["size"]:
+            c += 1
+        score *= abs(col - c)
+        # go west (young man)
+        c = col-1
+        while c > 0 and self.matrix[row][c]["size"] < tree["size"]:
+            c -= 1
+        score *= abs(col - c)
+
+        return score
 
     def file_name(self):
         return "../files/day08-trees.txt"
