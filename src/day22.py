b77172c7c9ee75eb7705e6f3ee56ff7002e4381a
molleafauss <lmollea@yahoo.it>
2022-12-26 21:55:24 +0100

    day 22 part 2 incomplete

--- src/day22.py
+++ src/day22.py
@@ -1,6 +1,20 @@
+from dataclasses import dataclass
+
 from advent import Solver
 
 # https://adventofcode.com/2022/day/21
+# TODO - INCOMPLETE
+# part 1 was approachable. Part 2 ... tough. Test input and real input had a different cube shape; I was unable to
+# find a way to calculate faces, adjacency and resulting rotation between them all in code, so I added some
+# pre-calculation to the input.
+# seems the result should be between 55k and 100k but atm the result is way lower. Stumped where the issue is
+
+DIRS = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0]
+]
 
 
 def parse_path(text):
@@ -17,11 +31,87 @@ def parse_path(text):
     return path
 
 
+@dataclass
+class Face:
+    id: int
+    row: int
+    col: int
+    size: int
+    facing: list
+
+    def contains(self, row, col):
+        return self.row <= row <= self.row + self.size and self.col <= col <= self.col + self.size
+
+    def relative(self, row, col):
+        return row - self.row, col - self.col
+
+    def cross(self, pos, dir, turn):
+        # pos[0] is diff in rows, pos[1] is diff in columns - one should be not relevant
+        match dir, turn:
+            case 0, 'R':
+                # crossing from the right to the top border
+                dir = (dir + 1) % len(DIRS)
+                return self.row, self.col + self.size - pos[0], dir
+            case 0, 'L':
+                # crossing from the right to the bottom border
+                dir = (dir - 1) % len(DIRS)
+                return self.row + self.size, self.col + pos[0], dir
+            case 0, 'U':
+                # crossing from the right to the right border (flipping)
+                dir = (dir + 2) % len(DIRS)
+                return self.row + self.size - pos[0], self.col + self.size, dir
+            case 1, 'R':
+                # crossing from the bottom to the right border
+                dir = (dir + 1) % len(DIRS)
+                return self.row + pos[1], self.col + self.size, dir
+            case 1, 'L':
+                # crossing from the bottom to the left border
+                dir = (dir - 1) % len(DIRS)
+                return self.row + pos[1], self.col, dir
+            case 1, 'U':
+                # crossing from the bottom to the bottom border
+                dir = (dir + 2) % len(DIRS)
+                return self.row + self.size, self.col + self.size - pos[1], dir
+            case 1, '=':
+                # bottom to top - no change in dir
+                return self.row, self.col + pos[1], dir
+            case 2, 'R':
+                # crossing from the left to the bottom border
+                dir = (dir + 1) % len(DIRS)
+                return self.row + self.size, self.col + pos[0], dir
+            case 2, 'L':
+                # crossing from the left to the top border
+                dir = (dir - 1) % len(DIRS)
+                return self.row, self.col + pos[0], dir
+            case 2, 'U':
+                # crossing from the left to the left border
+                dir = (dir + 2) % len(DIRS)
+                return self.row + self.size - pos[0], self.col, dir
+            case 3, 'R':
+                # crossing from the top to the right border
+                dir = (dir + 1) % len(DIRS)
+                return self.row + pos[1], self.col, dir
+            case 3, 'L':
+                # crossing from the top to the left border
+                dir = (dir - 1) % len(DIRS)
+                return self.row + pos[1], self.col, dir
+            case 3, 'U':
+                # crossing from the top to the top border
+                dir = (dir + 2) % len(DIRS)
+                return self.row, self.col + self.size - pos[1], dir
+            case 3, '=':
+                # top to bottom - no change in dir
+                return self.row + self.size, self.col + pos[1], dir
+        raise ValueError(f"Cannot determine where to go for {dir}, {turn} / {pos}")
+
+
 class Solution(Solver):
     def __init__(self):
         self.map = []
         self.path = None
         self.is_map = True
+        self.cube_size = 50
+        self.cube_faces = []
 
     def parse(self, line: str):
         if not line:
@@ -29,28 +119,66 @@ class Solution(Solver):
             return
         if self.is_map:
             self.map.append(line)
+        elif line.startswith("CUBE SIZE "):
+            self.cube_size = int(line[10:])
+        elif line.startswith("CUBE FACE "):
+            self.add_facing(line[10:])
         else:
             self.path = parse_path(line)
 
+    def find_faces(self):
+        # there is (should be) always one face in row 0
+        next_row = 0
+        next_col = 0
+        while len(self.cube_faces) < 6:
+            if next_col < len(self.map[next_row]) and self.map[next_row][next_col] != ' ':
+                face = Face(len(self.cube_faces) + 1, next_row, next_col, self.cube_size - 1, [])
+                self.cube_faces.append(face)
+                print(f"Found face {face.id} at {face.row}, {face.col}")
+            next_col += self.cube_size
+            if len(self.map[next_row]) < next_col:
+                next_row += self.cube_size
+                next_col = 0
+
+    def add_facing(self, text):
+        if not self.cube_faces:
+            self.find_faces()
+        id = int(text[0])
+        facing = eval(text[2:])
+        for f in self.cube_faces:
+            if f.id == id:
+                f.facing = facing
+                print(f"Face {id}: {facing}")
+
     def solve(self):
-        print(f"{self.path}")
+        print(f"Path: {len(self.path)} movements+turns")
         pos = [0, self.map[0].find("."), 0]
         print(f"Starting position: {pos}")
         for walk, turn in self.path:
             pos = self.walk(pos, walk)
             pos = self.turn(pos, turn)
-
         password = (pos[0] + 1) * 1000 + (pos[1] + 1) * 4 + pos[2]
         print(f"[1] final position: {pos} => password {password}")
 
-    def walk(self, pos, walk):
+        print("==> Cube Walk")
+        pos = [0, self.map[0].find("."), 0]
+        print(f"Starting position: {pos}")
+        for walk, turn in self.path:
+            pos = self.walk(pos, walk, True)
+            pos = self.turn(pos, turn)
+            # print(f"** {walk}, {turn} => {pos}")
+            pass
+        password = (pos[0] + 1) * 1000 + (pos[1] + 1) * 4 + pos[2]
+        print(f"[2] final position: {pos} => password {password}")
+
+    def walk(self, pos, walk, cube_walk=False):
         w = walk
         # rows are ordered top->bottom: v moves down = rows + 1; ^ moves up = rows -1
         while walk > 0:
             if pos[2] == 0 or pos[2] == 2:
-                nr, nc = self.move_col(pos[0], pos[1], pos[2])
+                nr, nc, npos = self.cube_walk(pos[0], pos[1], pos[2]) if cube_walk else self.move_col(pos[0], pos[1], pos[2])
             elif pos[2] == 1 or pos[2] == 3:
-                nr, nc = self.move_row(pos[0], pos[1], pos[2])
+                nr, nc, npos = self.cube_walk(pos[0], pos[1], pos[2]) if cube_walk else self.move_row(pos[0], pos[1], pos[2])
             if self.map[nr][nc] == '#':
                 # stop
                 break
@@ -58,8 +186,9 @@ class Solution(Solver):
             walk -= 1
             pos[0] = nr
             pos[1] = nc
+            pos[2] = npos
         assert self.map[pos[0]][pos[1]] == '.'
-        print(f"Walk {w} => {pos}")
+        # print(f"Walk {w} => {pos}")
         return pos
 
     def in_map(self, r, c):
@@ -72,7 +201,7 @@ class Solution(Solver):
         return True
 
     def move_col(self, r, c, dir):
-        dc = 1 if dir == 0 else -1
+        dc = DIRS[dir][1]
         c += dc
         row_len = len(self.map[r])
         while not self.in_map(r, c):
@@ -82,10 +211,10 @@ class Solution(Solver):
                 c = 0
             elif self.map[r][c] == ' ':
                 c += dc
-        return r, c
+        return r, c, dir
 
     def move_row(self, r, c, dir):
-        dr = 1 if dir == 1 else -1
+        dr = DIRS[dir][0]
         r += dr
         while not self.in_map(r, c):
             if r < 0:
@@ -97,7 +226,34 @@ class Solution(Solver):
                 r += dr
             elif self.map[r][c] == ' ':
                 r += dr
-        return r, c
+        return r, c, dir
+
+    def cube_walk(self, r0, c0, dir):
+        # only move one step
+        dr = DIRS[dir][0]
+        dc = DIRS[dir][1]
+        face = self.in_face(r0, c0)
+        r = r0 + dr
+        c = c0 + dc
+        if face.contains(r, c):
+            return r, c, dir
+        # select expected adjacent based on direction
+        adj = face.facing[dir]
+        if adj[1] is None:
+            print(f"Crossing face {face.id} / {adj[0]}: ({r0}, {c0}, {dir}) => ({adj[1]}) => ({r}, {c}, {dir})")
+            return r, c, dir
+        fadj = self.cube_faces[adj[0] - 1]
+        # cross into new face based on old position
+        pos = fadj.cross(face.relative(r0, c0), dir, adj[1])
+        assert fadj.contains(pos[0], pos[1])
+        print(f"Crossing face {face.id} / {fadj.id}: ({r0}, {c0}, {dir}) => ({adj[1]}) => {pos}")
+        return pos
+
+    def in_face(self, row, col):
+        for f in self.cube_faces:
+            if f.contains(row, col):
+                return f
+        raise ValueError(f"{row}, {col} not in any face?")
 
     def turn(self, pos, turn):
         facing = pos[2]
@@ -115,7 +271,7 @@ class Solution(Solver):
         elif facing > 3:
             facing = 0
         pos[2] = facing
-        print(f"Turn {turn} => {pos}")
+        # print(f"Turn {turn} => {pos}")
         return pos
 
     def file_name(self):
@@ -136,4 +292,11 @@ class Solution(Solver):
         ......#.
 
 10R5L5R10L4R5L5
+CUBE SIZE 4
+CUBE FACE 1: [[6, 'U'], [4, None], [3, 'L'], [2, 'U']]
+CUBE FACE 2: [[3, None], [5, 'U'], [6, 'R'], [1, 'U']]
+CUBE FACE 3: [[4, None], [5, 'L'], [2, None], [1, 'R']]
+CUBE FACE 4: [[6, 'R'], [5, None], [3, None], [1, None]]
+CUBE FACE 5: [[6, None], [2, 'U'], [3, 'R'], [4, None]]
+CUBE FACE 6: [[1, 'U'], [2, 'U'], [5, None], [4, 'L']]
 """
